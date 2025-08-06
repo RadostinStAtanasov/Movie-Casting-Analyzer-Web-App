5a6fe3fdeaa38103f87bb94b63fe3ca755e41fd8
RadostinStAtanasov <radostin.st.atanasov@gmail.com>
2025-08-06 11:32:05 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./assets/pages/Home";
+import MoviePage from "./assets/pages/Movie";
+import ActorPage from "./assets/pages/Actor";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "movies", element: <MoviePage /> },
+  { path: "actors", element: <ActorPage /> },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
